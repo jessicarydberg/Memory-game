cacb8f7d76fb52ccf32d51f23414a634e5ba505f
jessicarydberg <jessica.rydberg@hotmail.se>
2022-02-09 15:00:00 +0000

    Add eventlisteners to images and input

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,49 +3,60 @@
  */
 
 document.addEventListener("DOMContentLoaded", function() {
-    alert("hello");
-    console.log("hello")
+    let cards = document.getElementsByTagName("img");
+    let button = document.getElementsByTagName("input");
+
+    for (let card of cards) {
+        card.addEventListener("click", function() {
+            turnCard();
+        })
+    }
+
+    button[0].addEventListener("click", function() {
+        playGame();
+    })
+
 });
 
 /**
  * Starts a new game with random position of the images.
  */
 function playGame() {
-    console.log("you clicked play again!");
+    alert("hello from inside the playGame function!");
 }
 
 /**
  * Turns the card clicked on.
  */
 function turnCard() {
-    console.log("you clicked an image!");
+    alert("hello from inside the turnCard function!");
 }
 
 /**
  * Checks if the turned cards are a pair or not.
  */
 function checkPair() {
-    
+        alert("hello from inside the checkPair function!");
 }
 
 /**
  * Counts number of draws.
  */
 function drawCount() {
-
+    alert("hello from inside the drawCount function!");
 }
 
 /**
  * Keeps the record: The lowest drawcount for every game played sinse page loaded.
  */
 function recordCound() {
-
+        alert("hello from inside the recordCount function!");
 }
 
 /**
  * Congratulates when all pairs are found.
  */
 function gameFinished() {
-
+    alert("hello from inside the gameFinished function!");
 }
 
