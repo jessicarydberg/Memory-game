a5a1412295a97b0b25b78d4abd78e0ec6477c13b
jessicarydberg <jessica.rydberg@hotmail.se>
2022-02-09 08:13:50 +0000

    Name all functions and add dockstrings for description

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Starts a new game with random position of the images.
+ */
+function playgame()
+
+/**
+ * Turns the card clicked on.
+ */
+function turncard()
+
+/**
+ * Checks if the turned cards are a pair or not.
+ */
+function checkpair()
+
+/**
+ * Counts number of draws.
+ */
+function drawcount()
+
+/**
+ * Keeps the record: The lowest drawcount for every game played sinse page loaded.
+ */
+function recordcound()
+
+/**
+ * Congratulates when all pairs are found.
+ */
+function gamefinished()
+
