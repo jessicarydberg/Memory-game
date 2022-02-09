5b393c8a482c832736900c51fcf0a3f467105bda
jessicarydberg <jessica.rydberg@hotmail.se>
2022-02-09 10:14:09 +0000

    Solve problem with eventlistener for DOMContentLoaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,51 @@
 /**
+ * Get the image and button elements and add eventlisteners
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    alert("hello");
+    console.log("hello")
+});
+
+/**
  * Starts a new game with random position of the images.
  */
-function playgame()
+function playGame() {
+    console.log("you clicked play again!");
+}
 
 /**
  * Turns the card clicked on.
  */
-function turncard()
+function turnCard() {
+    console.log("you clicked an image!");
+}
 
 /**
  * Checks if the turned cards are a pair or not.
  */
-function checkpair()
+function checkPair() {
+    
+}
 
 /**
  * Counts number of draws.
  */
-function drawcount()
+function drawCount() {
+
+}
 
 /**
  * Keeps the record: The lowest drawcount for every game played sinse page loaded.
  */
-function recordcound()
+function recordCound() {
+
+}
 
 /**
  * Congratulates when all pairs are found.
  */
-function gamefinished()
+function gameFinished() {
+
+}
 
